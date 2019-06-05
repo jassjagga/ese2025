ed80614f9d42e87ccf2904f492bb4705ec93d837
jassjagga <jaswindersinghjagga@gmail.com>
2019-06-04 22:01:22 -0400

    Homework 2 (bubble sorting )

---
+++ coding/bubble_sort/bubble_sort.cpp
@@ -0,0 +1,46 @@
+/*
+ * bubble_sort.cpp
+ *
+ *  Created on: Jun. 4, 2019
+ *      Author: jass
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number[7];
+    int i,j;
+    cout << "Please enter any 7 numbers : -";
+    for(i=0;i<=6;i++)
+    {
+
+        cin>> number[i];
+    }
+    for (i=0;i<=5;i++)
+    {
+        for (j=i+1;j<=6;j++)
+        {
+            int temp;
+            if (number[i]<number[j])
+                {
+                    temp=number[i];
+                    number[i]= number[j];
+                    number[j]=temp;
+
+                }
+            }
+        }
+        for (i=0;i<=6;i++)
+        {
+            cout<<"=>"<<number[i]<<endl;
+
+        }
+        return(0);
+
+    }
+
+
+
+
