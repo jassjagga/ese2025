561d33330d00408485949d1825f5e8fc102141ad
Jaswinder Singh <51167676+jassjagga@users.noreply.github.com>
2019-06-26 11:39:37 -0400

    Update hello.cpp

--- coding/hello/src/hello.cpp
+++ coding/hello/src/hello.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : hello.cpp
-// Author      : jaswinder 
+// Author      : jaswinder singh
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
