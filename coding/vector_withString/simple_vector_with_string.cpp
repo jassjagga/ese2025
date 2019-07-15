ead92df05797a297ddb6682f5585bec4a1438257
jassjagga <jaswindersinghjagga@gmail.com>
2019-07-14 20:22:28 -0400

    String as a input and stored in vector

---
+++ coding/vector_withString/simple_vector_with_string.cpp
@@ -0,0 +1,41 @@
+//vector to store a string 
+// simple program to learn how to store string in vector
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+void print(vector<string>my_vec)
+{
+    
+    for(int i=0;i<my_vec.size();i++)
+    cout<<my_vec.at(i)<<" ";
+    
+}
+
+int main()
+{   
+      string words;
+      vector<string> my_vec;
+     cout<<"please enter any sentance =";
+     //while command is used to push back the every word of the sentence to the vector
+     while(cin>>words)
+     {
+       my_vec.push_back(words);
+     }
+    
+  //  my_vec.push_back(11);
+//    my_vec.push_back(10);
+  //  my_vec.push_back(8);
+    //my_vec.push_back(7);
+//    my_vec.push_back(6);
+    cout<<"The size of the array ="<< my_vec.size() <<endl;
+    cout<<"vector is =";
+    print(my_vec);
+    
+    
+    return 0;
+    
+    
+}
