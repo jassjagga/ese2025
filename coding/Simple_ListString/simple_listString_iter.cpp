827cda516b6441982f476f9e4414eece2d9e426a
jassjagga <jaswindersinghjagga@gmail.com>
2019-07-16 00:56:53 -0400

    simple Program to load string in the list container of C++

---
+++ coding/Simple_ListString/simple_listString_iter.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<list>
+#include<iterator>
+#include<string>
+
+using namespace std;
+
+list<string>::iterator iter;
+
+void printList(list<string> listWords)
+{   
+    cout<<endl;
+    cout<<" your entered sentence is= "<<endl;
+    
+    for(iter=listWords.begin();iter !=listWords.end();iter++)
+    {
+    cout<<" "<<*iter;
+    
+    }
+}
+
+
+int main()
+{
+    string words;
+    list<string>listWords;
+    
+    cout<<"Please enter any sentance= ";
+    
+    while(cin>>words)
+    {
+      listWords.push_back(words);
+    }
+    printList(listWords);
+    
+    return 0;
+    
+    
+}
