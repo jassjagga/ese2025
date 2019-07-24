8c77b5d93297abd47b965a723ac6657c70b6c549
jassjagga <jaswindersinghjagga@gmail.com>
2019-07-23 21:25:51 -0400

    Program to Read data from File and store it in vector

---
+++ coding/File_toVector/Debug/File_toVector.d
@@ -0,0 +1,1 @@
+File_toVector.o: ../File_toVector.cpp
---
+++ coding/File_toVector/File_toVector.cpp
@@ -0,0 +1,46 @@
+/*
+ * File_toVector.cpp
+ *
+ *  Created on: Jul. 23, 2019
+ *      Author: jass
+ */
+#include<iostream>
+#include<map>
+#include<string>
+#include<iterator>
+#include<fstream>
+#include<vector>
+
+using namespace std;
+vector<string>::iterator iter;
+
+void printVector(vector<string> myVec)
+{   cout<<"your Vector have this = ";
+    for(iter = myVec.begin();iter != myVec.end();iter++)
+    {
+        cout<<*iter;
+    }
+}
+
+int main()
+{
+    string words;
+
+    fstream myFile;
+    myFile.open("mytext.txt");
+
+    vector<string> myVec;
+    while(getline(myFile,words))
+    {
+        myVec.push_back(words);
+    }
+
+    printVector(myVec);
+
+    return 9;
+
+}
+
+
+
+
