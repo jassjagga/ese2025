3298fb1644a2914bf9bd8b9002b9c9a0564cd2ed
Jaswinder Singh <51167676+jassjagga@users.noreply.github.com>
2019-08-05 21:10:23 -0400

    line by line debugging(break points), header files,
    simple hello world program to learn about the breakpoint, header files.

--- coding/helloword/main.cpp
+++ coding/helloword/main.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
+//use of #pragma once will stop error here because we are including file twice while log.h conatins struct hye{} which cause dublication error 
 #include "log.h"
 #include "log.h"
 
 int main()
 {
+	//by setting breakpoint here (we and study the whole program)
+	
 	int a=8;
 	a++;
 	std::string b="hello";
@@ -13,7 +16,7 @@ int main()
 		const char c=b[i];
 		std::cout<<c<<std::endl;
 	}
-	
+	//instead of cout we make function describing it in other cpp file and accessing it by header file log.h 
 	logint();
 	log("Hello world!");
 	std::cin.get();
